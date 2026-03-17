7c2dfe36b0f9e48d634a99c2d96d693c7a654fc4
renatalorensini <renata.lorensini@escola.pr.gov.br>
2026-03-17 09:30:24 -0300

    Create Desafio1.py

---
+++ Desafio1.py
@@ -0,0 +1,11 @@
+# programa_soma.py
+
+# Solicita ao usuário dois números
+numero1 = float(input("Digite o primeiro número: "))
+numero2 = float(input("Digite o segundo número: "))
+
+# Soma os números
+soma = numero1 + numero2
+
+# Exibe o resultado
+print(f"A soma de {numero1} e {numero2} é {soma}")
