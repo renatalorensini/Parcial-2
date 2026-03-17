20f4ad495d279bb601ad871fec892a12131761e3
renatalorensini <renata.lorensini@escola.pr.gov.br>
2026-03-17 09:37:24 -0300

    Create Desafio3.py

---
+++ Desafio3.py
@@ -0,0 +1,9 @@
+# lista_nomes.py
+
+# Criando a lista de 5 nomes
+nomes = ["Renata", "Isane", "João", "Maria", "Gabriel"]
+
+# Imprimindo todos os nomes
+print("Lista de nomes:")
+for nome in nomes:
+    print(f"- {nome}")
